c1b4d527289452d9cadbed0e4b16d01699b6c27b
thisiscindy24 <cindyannwu@gmail.com>
2025-02-13 01:17:00 +0000

    2d10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/dice/2/6", { :controller => "dice", :action => "two_six" })
+  get("/dice/2/10", {:controller => "dice", :action=> "two_ten"})
 end
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
