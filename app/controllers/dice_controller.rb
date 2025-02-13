8d77af03aeddc2174961996633b36a500f6f58c3
thisiscindy24 <cindyannwu@gmail.com>
2025-02-13 01:28:15 +0000

    navigation

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -7,4 +7,14 @@ class DiceController < ApplicationController
     render({:template => "dice_templates/2d10"})
 
   end
+
+  def one_twenty
+    render({:template => "dice_templates/1d20"})
+
+  end
+
+  def five_four
+    render({:template => "dice_templates/5d4"})
+
+  end
 end
